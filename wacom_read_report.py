4d4e357d8ada634527851a44b4a514ae9bf7c3fc
Dylan Perry <dylan.perry@gmail.com>
2020-10-18 22:36:22 +1100

    🔨 Pretty print report data in hex

--- wacom_read_report.py
+++ wacom_read_report.py
@@ -1,3 +1,13 @@
+def pretty_print_hex(bytearray_):
+    width = 32
+    print("      ", " ".join(f"{col:02x}" for col in range(0, width)))
+
+    for line in range(0, len(bytearray_), width):
+        b = bytearray_[line:line + width]
+        print(f"{line:04x} | ", end="")
+        if b:
+            print(" ".join(f"{i:02x}" for i in b))
+
 
 def output_hidraw_reports():
     """
@@ -7,8 +17,9 @@ def output_hidraw_reports():
     with open("/dev/hidraw0", "rb+", buffering=0) as device:
         while True:
             report_data = device.read(1024)
-            print(f"Report data length: {len(report_data)}")
-            print(report_data.hex())
+            report_id = report_data[0]
+            print(f"Report ID: {report_id}, Report data length: {len(report_data) - 1}")
+            pretty_print_hex(report_data[1:])
 
 
 if __name__ == '__main__':
