d9d07a68c766c2844e475a58c72bc20555af550d
Dylan Perry <dylan.perry@gmail.com>
2020-10-19 12:22:31 +1100

    🔨 HID reports class to support future analysis
    Write output to file

--- wacom_read_report.py
+++ wacom_read_report.py
@@ -1,5 +1,8 @@
+from abc import abstractmethod, ABC
+from datetime import datetime
 from itertools import zip_longest
-from logging import getLogger, basicConfig
+from logging import getLogger, basicConfig, INFO
+from typing import List, Optional, TypeVar, Tuple
 
 
 def pretty_print_hex(bytearray_):
@@ -25,23 +28,89 @@ class SequenceDiff(object):
         return r
 
 
-def output_hidraw_reports():
+class HidReport:
+
+    def __init__(self, report_data: bytearray, use_report_id: bool = False) -> None:
+        super().__init__()
+        if use_report_id:
+            self._report_id = report_data[0]
+            self._data = report_data[1:]
+        else:
+            self._report_id = None
+            self._data = report_data.copy()
+        self._created_at = datetime.utcnow()
+
+    @property
+    def report_id(self) -> Optional[int]:
+        return self._report_id
+
+    def differing_bytes_indexes(self, other: 'HidReport') -> List[int]:
+        """
+        Ordered list of indexes where values of report data differ
+        """
+        if self._report_id != other._report_id:
+            raise ValueError(f"reports should have matching IDs ({self._report_id} != {other._report_id})")
+        return [n for n, (a, b) in enumerate(zip_longest(self._data, other._data, fillvalue=0)) if a != b]
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(...) <report_id = {self.report_id}, data = {len(self._data)} bytes, " \
+               f"created_at = {self._created_at.isoformat(timespec='microseconds')}> "
+
+    def __str__(self) -> str:
+        width = 16
+
+        def row(addr):
+            return " ".join(f"{i:02x}" for i in self._data[addr:addr + width])
+
+        return "".join([
+            "       ",
+        ] + [
+            f"{col:02x} " for col in range(0, width)
+        ] + [
+            "\n",
+        ] + [
+            f"{addr:04x} | {row(addr)}\n" for addr in range(0, len(self._data), width)
+        ])
+
+
+# T = TypeVar("T")
+#
+#
+# class RangeSet(ABC):
+#     @abstractmethod
+#     def __contains__(self, item: T) -> bool:
+#         pass
+#
+#     @abstractmethod
+#     def insert(self, item: T) -> 'RangeSet':
+#         pass
+
+
+def log_hidraw_reports():
     """
     Read reports input/feature from HIDRAW device and output to console
     """
-
-    d = SequenceDiff()
+    reports = []
 
     with open("/dev/hidraw0", "rb+", buffering=0) as device:
-        while True:
-            report_data = device.read(1024)
-            report_id = report_data[0]
-            log.info("Report ID: %d, data length: %d", report_id, len(report_data) - 1)
-            # pretty_print_hex(report_data[1:])
-            print(d.difference(report_data[1:]))
+        try:
+            while True:
+                report_data = bytearray(device.read(1024))
+                report = HidReport(report_data, True)
+                log.info("received report: %s", repr(report))
+                if report.report_id == 0x80:
+                    reports.append(report)
+        except KeyboardInterrupt:
+            log.info("keyboard interrupt, exiting")
 
+    # reports_diff = [reports[i].differing_bytes_indexes(reports[i + 1]) for i in range(0, len(reports) - 1)]
+
+    with open("hidraw.log", "w") as log_file:
+        for report in reports:
+            log_file.write(str(report))
+            log_file.write("\n")
 
 if __name__ == '__main__':
     log = getLogger(__name__)
-    basicConfig()
-    output_hidraw_reports()
+    basicConfig(level=INFO)
+    log_hidraw_reports()
