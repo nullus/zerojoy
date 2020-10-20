26ccfc94d704699d5349766c209a432449994ab5
Dylan Perry <dylan.perry@gmail.com>
2020-10-20 14:53:13 +1100

    🔧 Reduce data logged by wacom-read-report
    Don't log static bytes in HID report

--- src/zerojoy/wacom_read_report.py
+++ src/zerojoy/wacom_read_report.py
@@ -59,27 +59,31 @@ class HidReport:
                f"created_at = {self._created_at.isoformat(timespec='microseconds')}> "
 
     def __str__(self) -> str:
+        return self.hex_string()
+
+    @staticmethod
+    def hex_header() -> str:
+        width = 16
+
+        return "".join(["       "] + [f"{col:02x} " for col in range(0, width)] + ["\n"])
+
+    def hex_string(self, start=0, end=None):
         width = 16
 
         def row(addr):
-            return " ".join(f"{i:02x}" for i in self._data[addr:addr + width])
+            return " ".join(f"{i:02x}" for i in self._data[addr:min(addr + width, end or len(self._data))])
 
-        return "".join([
-            "       ",
-        ] + [
-            f"{col:02x} " for col in range(0, width)
-        ] + [
-            "\n",
-        ] + [
-            f"{addr:04x} | {row(addr)}\n" for addr in range(0, len(self._data), width)
-        ])
+        return "".join([f"{addr:04x} | {row(addr)}\n" for addr in range(start, end or len(self._data), width)])
 
 
 def log_hidraw_reports():
     """
     Read reports input/feature from HIDRAW device and output to console
     """
-    reports = []
+    reports: List[HidReport] = []
+
+    log = getLogger(__name__)
+    basicConfig(level=INFO)
 
     with open("/dev/hidraw0", "rb+", buffering=0) as device:
         try:
@@ -94,15 +98,22 @@ def log_hidraw_reports():
 
     reports_diff = [reports[i].differing_bytes_indexes(reports[i + 1]) for i in range(0, len(reports) - 1)]
 
-    report_ranges = reduce(lambda x, y: x.insert(y), chain(*reports_diff), Empty())
+    report_ranges = reduce(lambda x, y: x.insert(y), chain(*reports_diff), Empty()).ranges()
 
     with open("hidraw.log", "w") as log_file:
-        for report in reports:
-            log_file.write(str(report))
+        log_file.write(repr(reports[0]))
+        log_file.write("\n")
+        log_file.write(reports[0].hex_header())
+        log_file.write(reports[0].hex_string())
+        log_file.write("\n")
+        for report in reports[1:]:
+            log_file.write(repr(report))
+            log_file.write("\n")
+            log_file.write(report.hex_header())
+            for range_ in report_ranges:
+                log_file.write(report.hex_string(*range_))
             log_file.write("\n")
 
 
 if __name__ == '__main__':
-    log = getLogger(__name__)
-    basicConfig(level=INFO)
     log_hidraw_reports()
