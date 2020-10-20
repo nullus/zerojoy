00c7da2e8edc1c6d21b46f88a1f7b61e6be5669b
Dylan Perry <dylan.perry@gmail.com>
2020-10-20 16:19:39 +1100

    🔥 Remove unused functions

--- src/zerojoy/wacom_read_report.py
+++ src/zerojoy/wacom_read_report.py
@@ -7,29 +7,6 @@ from typing import List, Optional
 from zerojoy.collections import Empty
 
 
-def pretty_print_hex(bytearray_):
-    width = 32
-    print("      ", " ".join(f"{col:02x}" for col in range(0, width)))
-
-    for line in range(0, len(bytearray_), width):
-        b = bytearray_[line:line + width]
-        print(f"{line:04x} | ", end="")
-        if b:
-            print(" ".join(f"{i:02x}" for i in b))
-
-
-class SequenceDiff(object):
-
-    def __init__(self) -> None:
-        super().__init__()
-        self._previous = []
-
-    def difference(self, seq):
-        r = [n for n, (a, b) in enumerate(zip_longest(self._previous, seq, fillvalue=0)) if a ^ b]
-        self._previous = seq
-        return r
-
-
 class HidReport:
 
     def __init__(self, report_data: bytearray, use_report_id: bool = False) -> None:
